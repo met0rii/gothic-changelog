54c650953940706dfee33f50db6ecb6d592845cf
Ernest Rudnicki <e.rudnicki96@gmail.com>
2020-11-28 17:35:05 +0100

    vuex added

--- store/index.js
+++ store/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count: 0
+    mods: [],
+    error: false,
+    isLoading: false,
   },
   mutations: {
-    increment(state) {
-      state.count++
+    setMods(state, payload) {
+      state.mods = payload;
+    },
+    setLoading(state, isLoading) {
+      state.isLoading = isLoading;
+    },
+    setError(state, error) {
+      state.error = error
+    }
+  },
+  actions: {
+    getMods(context) {
+      context.commit("setLoading", true);
+      axios.get("/mock.json").then((res) => {
+        context.commit("setLoading", false);
+        context.commit("setMods", res.data);
+      }).catch((e) => {
+        context.commit("setLoading", false);
+        context.commit("setError", e.message);
+      })
     }
   }
 })
