5e6a81dedd07fde27c90269fb3cb93b7388aba45
Ernest Rudnicki <e.rudnicki96@gmail.com>
2020-11-28 16:45:45 +0100

    addd new page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark> </v-app-bar>
+    <v-navigation-drawer permanent color="black" dark app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Modifications </v-list-item-title>
+          <v-list-item-subtitle> See latest versions </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-    <v-main> </v-main>
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar dark app>
+      <v-toolbar-title>Wierza Ksardasa</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main class="content-container"> </v-main>
+
+    <v-footer dark app> </v-footer>
   </v-app>
 </template>
 
@@ -13,7 +36,31 @@ export default {
   components: {},
 
   data: () => ({
-    //
+    items: [
+      { title: "Mroczne tajemnice" },
+      { title: "Dzieje Khoronis" },
+      { title: "Edycja rozszerzona" },
+    ],
   }),
 };
 </script>
+
+<style scoped>
+.content-container {
+  background-color: #212121;
+  color: #fafafa;
+  height: 100%;
+}
+
+.recent-version {
+  margin-left: 3px;
+}
+
+.card-container {
+  margin-top: 16px;
+}
+
+.card:not(:first-child) {
+  margin-left: 12px;
+}
+</style>
---
+++ src/pages/ModChangelogs.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <h1>Mroczne tajemnice</h1>
+        <span class="recent-version">Latest version: 20-14-1996</span>
+      </v-col>
+    </v-row>
+    <v-divider dark></v-divider>
+    <v-row class="card-container">
+      <v-col cols="2" v-for="n in 10" v-bind:key="n">
+        <v-card dark class="card" color="grey darken-3" width="350">
+          <v-card-title class="headline">Changelog 20.13.2031</v-card-title>
+          <v-card-subtitle>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero
+            dicta fugiat maiores aliquam alias exercitationem minus pariatur
+          </v-card-subtitle>
+          <v-card-actions>
+            <v-btn text>Read more</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ModChangelogs",
+
+  components: {},
+
+  data: () => ({}),
+};
+</script>
+
+<style scoped>
+.recent-version {
+  margin-left: 3px;
+}
+
+.card-container {
+  margin-top: 16px;
+}
+
+.card:not(:first-child) {
+  margin-left: 12px;
+}
+</style>
