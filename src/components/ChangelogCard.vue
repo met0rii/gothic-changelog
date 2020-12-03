7ff3e948f1b8790b63badf2b12176a031e18f8c7
Ernest Rudnicki <e.rudnicki96@gmail.com>
2020-12-03 12:07:49 +0100

    headlines card fix

--- src/components/ChangelogCard.vue
+++ src/components/ChangelogCard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card dark class="card fill-height" color="grey darken-3" width="400">
     <v-card-title class="headline">Changelog {{ date }}</v-card-title>
-    <v-card-subtitle>
+    <v-card-subtitle class="subheadline">
       {{ description }}
     </v-card-subtitle>
     <v-card-actions class="card-actions">
@@ -33,6 +33,12 @@ export default {
 </script>
 
 <style scoped>
+.headline {
+  margin-bottom: 10px;
+}
+.subheadline {
+  margin-left: 2px;
+}
 .card {
   display: flex;
   flex-direction: column;
