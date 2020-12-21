be5a07736393e08a96b83181b916452407598093
Ernest Rudnicki <e.rudnicki96@gmail.com>
2020-12-21 15:05:12 +0100

    test

--- src/components/ChangelogCard.vue
+++ src/components/ChangelogCard.vue
@@ -2,7 +2,7 @@
   <v-card dark class="card fill-height" color="grey darken-3">
     <v-card-title class="headline">
       <router-link class="headline-link" :to="redirectData">
-        Changelog {{ version }}
+        Changelogt {{ version }}
       </router-link>
     </v-card-title>
     <v-card-subtitle class="subheadline">
