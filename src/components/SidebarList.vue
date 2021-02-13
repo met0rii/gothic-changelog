933d51766295dc2cd96db525d6b5052147e6f670
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2021-02-13 16:53:01 +0100

    Sidebar list item split into another component

--- src/components/SidebarList.vue
+++ src/components/SidebarList.vue
@@ -2,36 +2,30 @@
   <v-list class="custom-list-nav" dense nav>
     <div class="list-section">
       <div class="list-header">Główne</div>
-      <v-list-item class="custom-list-item" to="/" link>
-        <v-list-item-content>
-          <v-list-item-title>Strona główna</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <sidebar-list-item title="Strona główna" toRoute="/" />
     </div>
     <div class="list-section" v-for="(value, key) in sidebarItems" :key="key">
       <div class="list-header">
         {{ value.name }}
       </div>
-      <v-list-item
-        class="custom-list-item"
+      <sidebar-list-item
         v-for="child in value.data"
         :key="child.title"
-        link
-        :to="{
+        :title="child.title"
+        :toRoute="{
           name: 'Changelogs',
           params: { gameId: key, id: child.id },
         }"
-      >
-        <v-list-item-content>
-          <v-list-item-title v-text="child.title"></v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      />
     </div>
   </v-list>
 </template>
 
 <script>
+import SidebarListItem from "./SidebarListItem.vue";
 export default {
+  components: { SidebarListItem },
+  name: "SidebarList",
   props: {
     sidebarItems: Object,
   },
@@ -49,38 +43,5 @@ export default {
 .custom-list-nav.v-list--nav {
   padding: 0 !important;
   margin-top: 10px;
-}
-
-.custom-list-item.v-list-item {
-  padding: 4px !important;
-  margin-bottom: 0 !important;
-}
-
-.custom-list-item:hover {
-  border-radius: 0px;
-}
-.custom-list-item.v-list-item:before {
-  border-radius: 0px;
-}
-
-.custom-list-item.v-list-item--active {
-  color: var(--white-primary) !important;
-  border-radius: 0px !important;
-  border-right: 4px solid var(--orange-secondary);
-  background-color: rgba(255, 255, 255, 0.2) !important;
-}
-
-.custom-list-item .v-list-item__content {
-  min-height: 20px !important;
-  padding: 0px !important;
-  padding-left: 20px !important;
-}
-
-.custom-list-item .v-list-item__content .v-list-item__title {
-  font-size: 16px;
-}
-
-.custom-list-item.v-list-item--active::before {
-  opacity: 0.05;
 }
 </style>
---
+++ src/components/SidebarListItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-list-item class="custom-list-item" link :to="toRoute">
+    <v-list-item-content>
+      <v-list-item-title v-text="title"></v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+
+<script>
+export default {
+  name: "SidebarListItem",
+  props: {
+    title: String,
+    toRoute: [String, Object],
+  },
+};
+</script>
+
+<style scoped>
+.custom-list-item.v-list-item {
+  padding: 4px !important;
+  margin-bottom: 0 !important;
+}
+
+.custom-list-item:hover {
+  border-radius: 0px;
+}
+.custom-list-item.v-list-item:before {
+  border-radius: 0px;
+}
+
+.custom-list-item.v-list-item--active {
+  color: var(--white-primary) !important;
+  border-radius: 0px !important;
+  border-right: 4px solid var(--orange-secondary);
+  background-color: rgba(255, 255, 255, 0.2) !important;
+}
+
+.custom-list-item .v-list-item__content {
+  font-size: 20px !important;
+  min-height: 20px !important;
+  padding: 0px !important;
+  padding-left: 20px !important;
+}
+
+.custom-list-item .v-list-item__content .v-list-item__title {
+  font-size: 16px;
+}
+
+.custom-list-item.v-list-item--active::before {
+  opacity: 0.05;
+}
+</style>
