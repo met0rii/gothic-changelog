33b89f01886b283f4637d3dcb2b54316bf2e2d09
Sebastian Sobek <sebastian.sobek@witkac.pl>
2021-02-19 16:03:23 +0100

    - authors for disabled

--- src/views/mod-changelogs/changelogs/Changelogs.vue
+++ src/views/mod-changelogs/changelogs/Changelogs.vue
@@ -21,7 +21,13 @@
       </v-col>
     </v-row>
 
-    <v-row>
+    <v-row
+      v-if="
+        selected.authors &&
+          (selected.authors.creators.length > 0 ||
+            selected.authors.contributors.length > 0)
+      "
+    >
       <section-title title="Autorzy" />
       <v-col cols="12" class="pl-4 pr-4">
         <mod-authors :data="selected" />
