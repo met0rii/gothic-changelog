12e029d0e3d400df94f69cd0d3903891ce06c2c0
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2021-04-03 17:11:58 +0200

    google analytics added

--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
 import store from "../store/index";
+import VueGtag from "vue-gtag";
 
 // animations
 import './assets/css/vue2-animate.css';
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtag, {
+  config: { id: "G-70MWY7X2NK" }
+}, router);
 
 new Vue({
   store,
